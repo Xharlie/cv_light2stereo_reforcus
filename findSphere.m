74c28dcbb2b812990cd5d36f9d9a446fec0ab70a
xharlie <charlie.xu007@yahoo.com>
2017-11-08 18:41:11 -0500

    show findSphere

--- findSphere.m
+++ findSphere.m
@@ -1,1 +1,12 @@
 function [center, radius] = findSphere(img)
+    bw = img > 0;
+    stats = regionprops('table',bw,'Centroid',...
+    'MajorAxisLength','MinorAxisLength');
+    center = stats.Centroid
+    diameters = mean([stats.MajorAxisLength stats.MinorAxisLength],2);
+    radius = diameters/2
+    imshow(img)
+    hold on
+    viscircles(centers,radii);
+    
+end
