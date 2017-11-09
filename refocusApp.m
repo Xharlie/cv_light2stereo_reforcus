725af7ed6d7b3f92c44ddf619034aec99b494bec
xharlie <charlie.xu007@yahoo.com>
2017-11-09 00:17:54 -0500

    finish transition app

--- refocusApp.m
+++ refocusApp.m
@@ -1,1 +1,19 @@
 function refocusApp(rgb_stack, depth_map)
+    max_x = size(rgb_stack,2);
+    max_y = size(rgb_stack,1);
+    coordinate=[1,1];
+    f=figure()
+    while coordinate(1) < max_x && coordinate(1)>0 ...
+        && coordinate(2) < max_y && coordinate(2)>0 
+        depth_map(coordinate(2),coordinate(1));
+        rgb_stack(:,:,depth_map(coordinate(2),coordinate(1)) * 3-2 ...
+        : depth_map(coordinate(2),coordinate(1)) * 3);
+    
+        imshow(rgb_stack(:,:,depth_map(coordinate(2),coordinate(1)) * 3-2 ...
+        : depth_map(coordinate(2),coordinate(1)) * 3 ));
+        rgb_stack(:,:,depth_map(coordinate(2),coordinate(1)) * 3-2: depth_map(coordinate(2),coordinate(1)) * 3);
+        hold on;
+        coordinate = int8(ginput(1))
+    end
+    close(f);
+end
