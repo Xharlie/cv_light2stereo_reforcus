a5a724ac2100ed6227a0807f0690f6c0e6412790
xharlie <charlie.xu007@yahoo.com>
2017-11-08 20:22:25 -0500

    compute Mask, pass

--- computeMask.m
+++ computeMask.m
@@ -1,1 +1,9 @@
 function mask = computeMask(img_cell)
+    acc = img_cell{1};
+    for i = 2:5
+        acc = acc + img_cell{i};
+    end
+    mask = acc > 0;
+%     imshow(mask)
+%     hold on;
+end
