295b2d109cfafeddbe701657f4244e04b44b4eac
xharlie <charlie.xu007@yahoo.com>
2017-11-08 21:30:13 -0800

    challenge2a pass

--- generateIndexMap.m
+++ generateIndexMap.m
@@ -1,1 +1,19 @@
 function index_map = generateIndexMap(gray_stack, w_size)
+%     size(gray_stack);
+    lap_y=[0,1,0;0,-2,0; 0,1,0];
+    lap_x=lap_y.';
+    abs_lap_stack= abs(imfilter(gray_stack,lap_y)) + abs(imfilter(gray_stack,lap_x));
+%     size(abs_lap_stack);
+    h_sum = ones(w_size*2+1);
+    abs_sum_lap_stack = imfilter(abs_lap_stack,h_sum);
+%     size(abs_sum_lap_stack)
+    h_avg = fspecial('average',w_size*2+1);
+    abs_avg_sum_lap_stack = imfilter(abs_sum_lap_stack,h_avg);
+    index_map = zeros(size(abs_avg_sum_lap_stack(:,:,1)));
+    for i=1:size(index_map,1)
+        for j=1:size(index_map,2)
+            [argvalue, argmax] = max(abs_avg_sum_lap_stack(i,j,:));
+            index_map(i,j) = argmax;
+        end
+    end
+end
--- loadFocalStack.m
+++ loadFocalStack.m
@@ -1,1 +1,9 @@
 function [rgb_stack, gray_stack] = loadFocalStack(focal_stack_dir)
+    sample = imread([focal_stack_dir '/frame1.jpg']);
+    rgb_stack=zeros(size(sample,1),size(sample,2),3*25);
+    gray_stack=zeros(size(sample,1),size(sample,2),25);
+    for i = 1:25
+        rgb_stack(:,:,i*3-2:i*3) = imread([focal_stack_dir '/frame' num2str(i) '.jpg']);
+        gray_stack(:,:,i) = rgb2gray(imread([focal_stack_dir '/frame' num2str(i) '.jpg']));
+    end
+end
